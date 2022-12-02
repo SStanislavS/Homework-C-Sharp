4f209d0887d5818b339b00b49cb6d7419ce4bfbe
Stanislav <S@mail.ru>
2022-12-02 23:44:50 +0300

    add Task 5

---
+++ Task 4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа на входе принимает число (N), а на выходе показывает все четные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= n; i +=2)
+{
+    Console.Write("{0} ", i);
+    Console.WriteLine();
+}
