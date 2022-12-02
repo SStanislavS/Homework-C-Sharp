3560c4f3db58fbf34bc156e0be6292e5f54c28a8
Stanislav <S@mail.ru>
2022-12-02 11:13:58 +0300

    add Task 2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа на входе принимает три числа, а на выходе выдает какое число больше.
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("max = " +max);
